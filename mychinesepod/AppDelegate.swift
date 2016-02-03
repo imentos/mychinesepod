ee1af8a8c9e6c966077d572cf3aa63c320932de1
Ray Kuo <ray.kuo@sap.com>
2016-02-03 15:51:49 -0800

    first commits

---
+++ mychinesepod/AppDelegate.swift
@@ -0,0 +1,30 @@
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+    
+    var window: UIWindow?
+    
+    var appController: TVApplicationController?
+    static let TVBaseURL = "https://s3-us-west-2.amazonaws.com/mychinesepod/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        // 1
+        let appControllerContext = TVApplicationControllerContext()
+        
+        // 2
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else {
+            fatalError("unable to create NSURL")
+        }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        // 3
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+}
